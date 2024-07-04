d2986be87a534e52ce31a17bed7d425fb13f6bee
Jacob Castanon <jacastanon01@gmail.com>
2024-07-04 15:47:58 -0500

    working with computer vision. Experimetning with different methods to best prepare image for OCR

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import pprint
+import pdf2image
+import pdf2image.parsers
 import pytesseract
 import cv2
 
@@ -74,6 +76,7 @@ def parse_text_to_struct(lines):
 
             line = lines[i].strip()
             if line and (line in label or label in line):
+
                 print(line, end="!!!!!!!!\n")
                 i += 1
                 if lines[i].strip() in label or label in lines[i].strip():
@@ -101,34 +104,126 @@ def parse_text_to_struct(lines):
     return parsed_data
 
 
+def data_to_dict(data: list[str]) -> dict[str, str] | None:
+    print([d for d in data if d])
+    expected_fields = {
+        "age": "AGE",
+        "name": "NAME",
+        "sex": "SEX",
+        "buried_info": "WHERE BURIED | DATE OF BURIAL",
+        "cause_of_death": "CAUSE OF DEATH",
+        "late_residence": "LATE RESIDENCE",
+        "undertaker": "UNDERTAKER",
+        "remarks": "REMARKS",
+    }
+
+    parsed_data = {key: "" for key in expected_fields.keys()}
+    current_kw = None
+
+    i = 0
+    field_i = 0
+    while i < len(data):
+        line = data[i].strip()
+
+        if line == "":
+            i += 1
+            continue
+
+        for key, label in expected_fields.items():
+            if line.lower() in label.lower():
+                current_kw = key
+                i += 1
+                value = []
+
+                while (
+                    i < len(data)
+                    and data[i].strip() != ""
+                    and not any(
+                        [data[i] in lbl.lower() for lbl in expected_fields.values()]
+                    )
+                ):
+                    value.append(data[i].strip())
+                    i += 1
+
+                parsed_data[current_kw] = " ".join(value)
+                break
+        else:
+            i += 1
+
+    parsed_data = {k: v for k, v in parsed_data.items() if v}
+
+    # pprint.pp(parsed_data, compact=False, indent=4)
+    if not parsed_data:
+        return None
+
+    return parsed_data
+
+
 def extract_and_parse_text(pdf_path):
-    my_config = r"--oem 3 --psm 3"
-    pages = convert_from_path(pdf_path, 300)
-    image_path = generate_jpg_file("pdf_img", pages[1])
+    my_config = r"--oem 3 --psm 3"  # 3 4 6 11
+    page = convert_from_path(pdf_path, 300, single_file=True, grayscale=True)[0]
 
-    image = cv2.imread(image_path)
-    data = pytesseract.image_to_data(
-        image, lang="eng", config=my_config, output_type=Output.DICT
+    image_path = generate_jpg_file(f"pdf_img", page)
+
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    image = cv2.fastNlMeansDenoising(image, h=30)
+    image = cv2.GaussianBlur(image, (5, 5), 2)
+    image = cv2.adaptiveThreshold(
+        image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2
     )
-    n_boxes = len(data["level"])
 
-    for i in range(n_boxes):
-        (x, y, w, h) = (
-            data["left"][i],
-            data["top"][i],
-            data["width"][i],
-            data["height"][i],
-        )
-        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    cv2.imwrite("threshold.jpg", image)
 
-    found_text = data["text"]
+    # generate_jpg_file("threshold", image)
 
-    if found_text is not None:
-        parsed_text = parse_text_to_struct(found_text)
-    pprint.pp(parsed_text, indent=2)
+    # Apply morphological transformations
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    morph = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+
+    _, contours = cv2.findContours(morph, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for c in contours:
+        if c.any(None) or len(c) < 5:
+            continue
+        # get properties of contour and assign region of interest
+        x, y, w, h = cv2.boundingRect(c)
+        print(c)
+        if len(c) > 4 or w > 10 and h > 10:
+            roi = image[y : y + h, x : x + w]
+            cv2.imshow("ROI", roi)
+            cv2.waitKey(0)
+            text = pytesseract.image_to_data(roi, lang="eng", config=my_config)
+            print(f"#########\n{text}\n")
+            # if text is not None:
+            #     # parsed_text = parse_text_to_struct(found_text)
+            #     parsed_text = data_to_dict(text)
+
     # cv2.imshow("img", image)
     # cv2.waitKey(0)
 
+    data = pytesseract.image_to_string(
+        image, lang="eng", config=my_config, output_type=Output.DICT
+    )
+
+    # n_boxes = len(data["level"])
+
+    # for i in range(n_boxes):
+    #     (x, y, w, h) = (
+    #         data["left"][i],
+    #         data["top"][i],
+    #         data["width"][i],
+    #         data["height"][i],
+    #     )
+    #     cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    found_text = data["text"]
+    print(f"FOUND TEXT: ", found_text.strip())
+
+    # pprint.pp(parsed_text, indent=2)
+    cv2.imshow("img", image)
+    cv2.waitKey(0)
+
 
 if __name__ == "__main__":
-    extract_and_parse_text("test.pdf")
+    extract_and_parse_text("test_lines.pdf")
+#
---
+++ process_image.py
@@ -0,0 +1,148 @@
+from typing import final
+import numpy as np
+import cv2
+import os
+from pdf2image import convert_from_path
+import pytesseract
+
+
+def generate_jpg_file(filename: str, image) -> str:
+    """creates images folder if one doesn't exist and generates jpg file
+
+    Args:
+        filename (str): Name of jpg file
+        image (Image): PIL Image Object to save to file
+
+    Returns:
+        str: absolute path to generated jpg
+    """
+    dirpath = f"{os.getcwd()}/images"
+
+    if not os.path.exists(dirpath):
+        os.mkdir(dirpath)
+
+    abs_image_path = os.path.join(f"{dirpath}/{filename}.jpg")
+    if os.path.exists(abs_image_path):
+        os.remove(abs_image_path)
+        print(f"{filename.split("/")[-1]} deleted")
+    image.save(abs_image_path, "JPEG")
+    return abs_image_path
+
+
+def process_image():
+    bw_path = "images/im_bw.jpg"
+    no_noise_path = "images/no_noise.jpg"
+
+    my_config = r"--oem 3 --psm 3"  # 3 4 6 11
+    pdf_path = "test_remarks.pdf"
+
+    page = convert_from_path(pdf_path, 300, single_file=True, grayscale=True)[0]
+    im_path = generate_jpg_file("page", page)
+
+    image = cv2.imread(im_path, cv2.IMREAD_GRAYSCALE)
+    cv2.imwrite(im_path, image)
+
+    inverse = cv2.bitwise_not(image)
+    cv2.imwrite("inverse.jpg", inverse)
+
+    # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # cv2.imwrite("grey.jpg", image)
+
+    # kernel = np.ones((5, 5), np.uint8)
+    # erosion = cv2.erode(grey, kernel, iterations=1)
+    # cv2.imwrite("erosion.jpg", erosion)
+
+    threshold, im_bw = cv2.threshold(image, 200, 235, cv2.THRESH_BINARY)
+
+    cv2.imwrite(bw_path, im_bw)
+
+    # im_bw = adapt_thresh(image)
+    im_bw = adapt_thresh(image)
+    # text = pytesseract.image_to_string(im_bw, lang="eng", config=my_config)
+    # print(f"Final Text: ", text, end="\n\n\n")
+
+    dilation = remove_noise(image)
+    cv2.imwrite(no_noise_path, dilation)
+    # contours, hierarchy = cv2.findContours(
+    #     dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
+    # )
+    # print(contours)
+    # print(hierarchy)
+    # cnts = sorted(contours, key=lambda c: cv2.boundingRect(c)[0])
+    # for i in range(len(cnts)):
+    #     x, y, w, h = cv2.boundingRect(cnts[i])
+    #     cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0))
+    # cv2.imwrite("rectangle.jpg", inverse)
+
+    # cv2.imwrite("images/final.jpg", image)
+
+    ero_im_thin = thin_or_thick_font(im_bw)
+    # im_thick = thin_or_thick_font(im_bw, thick=True)
+    cv2.imwrite("images/erode.jpg", ero_im_thin)
+
+    for i in (im_bw, ero_im_thin, dilation):
+        text = pytesseract.image_to_string(i, lang="eng", config=my_config)
+        print(f"Final Text {i}: ", text, end="\n\n\n")
+
+    # cv2.imwrite("images/dilate.jpg", im_thick)
+    # final_text = pytesseract.image_to_string(im_bw, lang="eng", config=my_config)
+    # eroded_text = pytesseract.image_to_string(ero_im_thin, lang="eng", config=my_config)
+    # dilated_text = pytesseract.image_to_string(im_thick, lang="eng", config=my_config)
+    # dilated_text = pytesseract.image_to_string(dilation, lang="eng", config=my_config)
+    # grey_text = pytesseract.image_to_string(grey, lang="eng", config=my_config)
+    # print("Eroded Text: ", eroded_text)
+    # print("Dilated Text: ", dilated_text)
+    # print("Grey Text: ", grey_text)
+
+
+def remove_noise(image):
+    image = cv2.bitwise_not(image)
+    kernel = np.ones([2, 2], np.uint8)
+    image = cv2.dilate(image, kernel, iterations=1)
+    kernel = np.ones([1, 1], np.uint8)
+    image = cv2.erode(image, kernel, iterations=1)
+    image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+    image = cv2.medianBlur(image, 3)
+    image = cv2.bitwise_not(image)
+    return image
+
+
+def adapt_thresh(image):
+    # image = cv2.bitwise_not(image)
+    image = cv2.fastNlMeansDenoising(image, h=10)
+    image = cv2.GaussianBlur(image, (3, 3), 2)
+    image = cv2.adaptiveThreshold(
+        image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2
+    )
+
+    image = cv2.bitwise_not(image)
+    image = cv2.dilate(image, np.ones([2, 2], np.uint8), iterations=1)
+    image = cv2.erode(image, np.ones([1, 1], np.uint8), iterations=5)
+    image = cv2.bitwise_not(image)
+
+    cv2.imwrite("images/threshold.jpg", image)
+    return image
+
+
+def thin_or_thick_font(image, thick: bool = False):
+    """thicken or thin text in image
+
+    Args:
+        image (MatLike): image object
+        thick (bool, optional=False): Determines whether to thicken (dilate) or thin (erode) text
+
+    Returns:
+        _type_: _description_
+    """
+    image = cv2.bitwise_not(image)
+    kernel = np.ones((2, 2), np.uint8)
+    # if thick:
+    image = cv2.dilate(image, kernel, iterations=1)
+    # else:
+    image = cv2.erode(image, kernel, iterations=1)
+    image = cv2.bitwise_not(image)
+    return image
+
+
+if __name__ == "__main__":
+    process_image()
