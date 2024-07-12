50e93c4c10eaeb1023f71216a97fbd155e7e3e19
Jacob Castanon <jacastanon01@gmail.com>
2024-07-12 15:56:59 -0600

    various changes made to processing function. Changed PSM to 6

--- process_image.py
+++ process_image.py
@@ -6,7 +6,7 @@ import pytesseract
 
 
 def calulcate_white_px(image) -> int:
-    """Calculates numnber of white pixels in image
+    """Calculates number of white pixels in image
 
     Args:
         image (Numpy Array):
@@ -19,13 +19,16 @@ def calulcate_white_px(image) -> int:
     return cv2.countNonZero(image) / (height * width)
 
 
-def process_image(pdf_path: str):
+def process_image(pdf_path: str) -> str:
     """Takes pdf filepath and converts to image to perform processing
 
     Args:
         pdf_path (str): path... to the pdf file
+
+    Returns:
+        text (str): result of tesserect OCR conversion to string
     """
-    my_config = r"--oem 3 --psm 3"  # 3 4 6 11
+    my_config = r"--oem 3 --psm 6"  # 3 4 6 11
 
     pages = convert_from_path(
         pdf_path,
@@ -40,11 +43,12 @@ def process_image(pdf_path: str):
 
     page = pages[0] if first_white < second_white else pages[1]
 
-    im_path = generate_jpg_file("name", page)
-    image = cv2.imread(im_path)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    save_page_path = "images/name.jpg"
 
-    assert image is not None
+    page.save(save_page_path, "JPEG")
+
+    image = cv2.imread(save_page_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     im_bw = adapt_thresh(image)
     # threshold, im_bw = cv2.threshold(
@@ -52,55 +56,34 @@ def process_image(pdf_path: str):
     # )  # cv2.THRESH_OTSU
 
     cv2.imwrite("images/im_bw.jpg", im_bw)
-    ntext = pytesseract.image_to_string(im_bw, lang="eng", config=my_config)
-    print(ntext)
+    text = pytesseract.image_to_string(im_bw, lang="eng", config=my_config)
+    # print(text)
     cv2.destroyAllWindows()
+    return text
 
 
 def adapt_thresh(image):
     "Manipulates image to be more readable to OCR"
-    kernel_dilate = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 1))
-    kernel_erode = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
+    # kernel_dilate = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 1))
+    # kernel_erode = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
 
     image = image.copy()
 
     image = cv2.bitwise_not(image)
-    image = cv2.fastNlMeansDenoising(image, h=40)
-    image = cv2.GaussianBlur(image, (3, 3), 0)
+    image = cv2.fastNlMeansDenoising(image, h=60)
+    image = cv2.GaussianBlur(image, (7, 7), 0)
     cv2.imwrite("images/blur.jpg", image)
-    image = cv2.dilate(image, kernel_dilate, iterations=1)
-    image = cv2.erode(image, kernel_erode, iterations=1)
+    # image = cv2.dilate(image, kernel_dilate, iterations=1)
+    # image = cv2.erode(image, kernel_erode, iterations=1)
     image = cv2.bitwise_not(image)
     image = cv2.adaptiveThreshold(
-        image, 240, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 5
+        image, 240, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 3
     )
 
     cv2.imwrite("images/threshold.jpg", image)
     return image
 
 
-def generate_jpg_file(filename: str, image) -> str:
-    """creates images folder if one doesn't exist and generates jpg file
-
-    Args:
-        filename (str): Name of jpg file
-        image (Image): PIL Image Object to save to file
-
-    Returns:
-        str: absolute path to generated jpg
-    """
-    dirpath = f"{os.getcwd()}/images"
-
-    if not os.path.exists(dirpath):
-        os.mkdir(dirpath)
-
-    abs_image_path = os.path.join(f"{dirpath}/{filename}.jpg")
-    if os.path.exists(abs_image_path):
-        os.remove(abs_image_path)
-        print(f"{filename.split("/")[-1]} deleted")
-    image.save(abs_image_path, "JPEG")
-    return abs_image_path
-
-
 if __name__ == "__main__":
-    process_image("test_remarks.pdf")
+    text = process_image("test_lines.pdf")
+    print(text)
