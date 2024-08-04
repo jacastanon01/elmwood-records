28087c74f383a7427aead5f2add8f24dfc4f42c1
Jacob Castanon <jacastanon01@gmail.com>
2024-08-04 15:51:31 -0500

    More fun with WebDrivers. Set up firefox profile, service and geckodrive manager to cache sessions/cookies for auth

--- webdriver.py
+++ webdriver.py
@@ -1,14 +1,37 @@
 from selenium import webdriver
+import os
 
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.chrome.options import Options
+# from webdriver_manager.chrome import ChromeDriverManager
 
-# from selenium.webdriver.firefox.options import Options
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import SessionNotCreatedException
 
 URL = "https://ckonline.tbgtom.com/DatabaseEdit.aspx?id=0"
+
+xpaths = {
+    "block": '//*[@id="ctl00_ContentPlaceHolder1_txtSection"]',
+    "lot": '//*[@id="ctl00_ContentPlaceHolder1_txtLot"]',
+    "grave": '//*[@id="ctl00_ContentPlaceHolder1_txtGrave"]',
+    "inter_date": '//*[@id="ctl00_ContentPlaceHolder1_txtInterDate"]',
+    "cremains": '//*[@id="ctl00_ContentPlaceHolder1_IsCremains"]',
+    "first_name": '//*[@id="ctl00_ContentPlaceHolder1_txtInterFirstName"]',
+    "last_name": '//*[@id="ctl00_ContentPlaceHolder1_txtInterLastName"]',
+    "gender": '//*[@id="ctl00_ContentPlaceHolder1_cboGender"]',
+    "undertaker": '//*[@id="ctl00_ContentPlaceHolder1_txtUndertaker"]',
+    "death_cause": '//*[@id="ctl00_ContentPlaceHolder1_txtDeathCause"]',
+    "res_address": '//*[@id="ctl00_ContentPlaceHolder1_cfLate_Residence_Address"]',
+    "res_city": '//*[@id="ctl00_ContentPlaceHolder1_cfLate_Residence_City"]',
+    "res_state": '//*[@id="ctl00_ContentPlaceHolder1_cfLate_Residence_State"]',
+    "initials": '//*[@id="ctl00_ContentPlaceHolder1_cfInitials"]',
+    "age_at_death": '//*[@id="ctl00_ContentPlaceHolder1_cfAge_at_Death"]',
+    "plot_card_num": '//*[@id="ctl00_ContentPlaceHolder1_cfPlot_Card_Num"]',
+}
 
 
 # def chrome_driver():
@@ -30,17 +53,16 @@ URL = "https://ckonline.tbgtom.com/DatabaseEdit.aspx?id=0"
 #     except SessionNotCreatedException as e:
 #         print(e)
 
+profile_path = os.getenv("PROFILE_PATH", "")
 
-options = Options()
-options.add_experimental_option("detach", True)
-
-
-driver = webdriver.Chrome(
-    service=Service(ChromeDriverManager().install()), options=options
-)
+ff_options = webdriver.FirefoxOptions()
+profile = FirefoxProfile(profile_path)
+ff_options.profile = profile
+service = Service(GeckoDriverManager().install(), options=ff_options)
+driver = webdriver.Firefox(service=service)
 
 driver.get(URL)
-driver.maximize_window()
+assert "Crypt Keeper" in driver.title
 
 element = driver.find_element(
     By.XPATH, '//*[@id="ctl00_ContentPlaceHolder1_txtSection"]'
